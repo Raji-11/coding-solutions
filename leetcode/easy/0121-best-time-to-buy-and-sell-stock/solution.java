125d634cf9d73dd1b6fbe7ca4451269e5a876981
Rajasri yasaswini B <23a81a05e7@sves.org.in>
2026-09-03 21:46:13 +0530

    ✅ Best Time to Buy and Sell Stock | Easy | Java | Runtime: 2 ms (44.48%) | Memory: 94.6 MB | 2026-09-03

--- leetcode/easy/0121-best-time-to-buy-and-sell-stock/solution.java
+++ leetcode/easy/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
+        int mini=prices[0];
         int maxprofit=0;
-        int min=prices[0];
         for(int i=1;i<n;i++){
-            int cost=prices[i]-min;
+            int cost=prices[i]-mini;
             maxprofit=Math.max(maxprofit,cost);
-            min=Math.min(prices[i],min);
+            mini=Math.min(mini,prices[i]);
         }
         return maxprofit;
     }
