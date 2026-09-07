d440a286b1b0e435ee089c480a29905994057a28
Rajasri yasaswini B <23a81a05e7@sves.org.in>
2026-09-07 20:57:46 +0530

    ✅ Maximum Average Subarray I | Easy | Java | Runtime: 5 ms (32.25%) | Memory: 69.8 MB | 2026-09-07

--- leetcode/easy/0643-maximum-average-subarray-i/solution.java
+++ leetcode/easy/0643-maximum-average-subarray-i/solution.java
@@ -2,17 +2,25 @@ class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int n=nums.length;
         int sum=0;
-        int max=Integer.MIN_VALUE;
+        double avg=0;
+        double maxavg=0;
         for(int i=0;i<k;i++){
             sum+=nums[i];
+            
         }
-        max=sum;
-        for(int i=k;i<n;i++){
-            sum=sum-nums[i-k]+nums[i];
-            max=Math.max(max,sum);
+        avg=(double)sum/k;
+        maxavg=avg;
+        int l=0,r=k-1;
+        while(r<n-1){
+            sum-=nums[l];
+            l++;
+            r++;
+            sum=sum+nums[r];
+            avg=(double)sum/k;
+            maxavg=Math.max(maxavg,avg);
         }
-       
-        return (double) max/k;
+        
+        return maxavg;
         
     }
 }
