9935d210ebabd6c71ba57f1f14ce0a470fc8d047
Rajasri yasaswini B <23a81a05e7@sves.org.in>
2026-09-13 13:18:24 +0530

    ✅ Valid Parentheses | Easy | Java | Runtime: 4 ms (36.19%) | Memory: 43.2 MB | 2026-09-13

---
+++ leetcode/easy/0020-valid-parentheses/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n=s.length();
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='['||ch=='{'){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top=stack.peek();
+                if((ch==')'&&top=='(')||(ch==']'&&top=='[')||(ch=='}'&&top=='{')){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+        
+    }
+}
