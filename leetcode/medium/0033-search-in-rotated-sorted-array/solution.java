0e612be11ce0c08c3ff3b7b61df494ad44568ea7
Rajasri yasaswini B <23a81a05e7@sves.org.in>
2026-09-16 23:20:14 +0530

    ✅ Search in Rotated Sorted Array | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 43.9 MB | 2026-09-16

--- leetcode/medium/0033-search-in-rotated-sorted-array/solution.java
+++ leetcode/medium/0033-search-in-rotated-sorted-array/solution.java
@@ -1,9 +1,24 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n=nums.length;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-               return i;
+        int l=0,h=n-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(nums[m]==target){
+                return m;
+            }else if(nums[l]<=nums[m]){
+                if(nums[l]<=target&&target<nums[m]){
+                    h=m-1;
+                }else{
+                    l=m+1;
+                }
+
+            }else{
+                if(nums[m]<target&&target<=nums[h]){
+                    l=m+1;
+                }else{
+                    h=m-1;
+                }
             }
         }
         return -1;
