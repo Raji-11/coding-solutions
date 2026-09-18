a7476a2c2e1f636aeaa83e1556604e7a9e48c027
Rajasri yasaswini B <23a81a05e7@sves.org.in>
2026-09-18 16:24:12 +0530

    ✅ Single Number | Easy | Java | Runtime: 14 ms (14.44%) | Memory: 46.9 MB | 2026-09-18

---
+++ leetcode/easy/0136-single-number/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n=nums.length;
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        for(int i=0;i<n;i++){
+            hs.put(nums[i],hs.getOrDefault(nums[i],0)+1);
+        }
+        for(int x:hs.keySet()){
+            if(hs.get(x)==1){
+                return x;
+            }
+        }
+        return -1;
+    }
+}
