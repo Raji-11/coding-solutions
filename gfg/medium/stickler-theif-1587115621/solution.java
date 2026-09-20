66cbccf96e23e40a2c205ff7ca85b09837fb12bf
Rajasri yasaswini B <23a81a05e7@sves.org.in>
2026-09-20 19:25:59 +0530

    ✅ Stickler Thief | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ gfg/medium/stickler-theif-1587115621/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMaxSum(int arr[]) {
+        // code here
+        int n=arr.length;
+        int dp[]=new int[n];
+        dp[0]=arr[0];
+        dp[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],arr[i]+dp[i-2]);
+        }
+        return dp[n-1];
+    }
+}
