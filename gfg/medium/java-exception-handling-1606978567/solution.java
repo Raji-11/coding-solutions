41220855fd81519b54ba7a8526fcc32faa55bb5f
Rajasri yasaswini B <23a81a05e7@sves.org.in>
2026-09-20 23:08:14 +0530

    ✅ java-exception-handling-1606978567 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ gfg/medium/java-exception-handling-1606978567/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findMin(int a, int b) {
+        // code here
+        int result = a + b;
+
+                try {
+                    if (a - b < result) {
+                        result = a - b;
+                    }
+
+                    if (a * b < result) {
+                        result = a * b;
+                    }
+
+                    int div = a / b;
+
+                    if (div < result) {
+                        result = div;
+                    }
+                } catch (ArithmeticException e) {
+                    // Division by zero occurred
+                }
+
+                return result;
+    }
+}
