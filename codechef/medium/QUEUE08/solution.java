1ba12608aac15bb10c8b62d2d92272e9146b3a77
Rajasri yasaswini B <23a81a05e7@sves.org.in>
2026-09-26 10:49:02 +0530

    ✅ QUEUE08 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-26

---
+++ codechef/medium/QUEUE08/solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int k=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    for(int i=0;i<n;i++){
+		        arr[i]=sc.nextInt();
+		    }
+		    k=k%n;
+		    StringBuilder sb=new StringBuilder();
+		    for(int i=k;i<n;i++){
+		        sb.append(arr[i]).append(" ");
+		    }
+		    for(int i=0;i<k;i++){
+		        sb.append(arr[i]).append(" ");
+		    }
+		    System.out.println(sb.toString().trim());
+		    
+		}
+		sc.close();
+
+	}
+}
